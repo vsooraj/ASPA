3ad5e9fc104dd7f2b3fe2786510d1ba614f7ff49
jesmi.pk <jesmi.pk@CS-044-DT-WS>
2017-01-20 12:42:54 +0530

    [ASPA:02.1] Added TopNav Bar,Excluded _Layout and _ViewStart html pages

---
+++ ASPA.Web/Scripts/spa/layout/topBar/topBar.directive.js
@@ -0,0 +1,14 @@
+﻿(function (app) {
+	'use strict';
+
+	app.directive('topBar', topBar);
+
+	function topBar() {
+		return {
+			restrict: 'E',
+			replace: true,
+			templateUrl: '~/Scripts/spa/layout/topBar/topBar.html'
+		}
+	}
+
+})(angular.module('common.ui'));
